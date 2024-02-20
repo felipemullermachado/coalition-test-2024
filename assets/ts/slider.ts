775f88e80265118091b3a554cf84093c4c77117e
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-02-20 16:09:00 -0300

    css mobile version

--- assets/ts/slider.ts
+++ assets/ts/slider.ts
@@ -8,5 +8,19 @@ export default function () {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ]
   })
 }
