98e588903ef3a65d3f69042d66607e85c8f17a7a
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-02-20 17:10:41 -0300

    accordion component

--- assets/ts/accordionsAndTabs.ts
+++ assets/ts/accordionsAndTabs.ts
@@ -1,15 +1,7 @@
 import $ from "jquery";
 
 export default function () {
-  if ($(window).width() < 768) {
-    $(".tabs").hide();
-    $(".accordion").show();
-  } else {
-    $(".tabs").show();
-    $(".accordion").hide();
-  }
-
-  $(window).resize(function () {
+  function toggleTabsAccordion() {
     if ($(window).width() < 768) {
       $(".tabs").hide();
       $(".accordion").show();
@@ -17,10 +9,16 @@ export default function () {
       $(".tabs").show();
       $(".accordion").hide();
     }
+  }
+
+  $(window).resize(function () {
+    toggleTabsAccordion();
   });
 
+  toggleTabsAccordion();
+
   $(".tab").click(function () {
-    var targetContent = $(this).attr("aria-controls");
+    let targetContent = $(this).attr("aria-controls");
     $(".tab").removeClass("active").attr("aria-selected", "false");
     $(this).addClass("active").attr("aria-selected", "true");
     let classes = $(".bkg-climb").attr("class").split(" ");
@@ -30,7 +28,16 @@ export default function () {
     $("#" + targetContent).show();
   });
 
+  $(".accordion .accordion-item:not(:first-of-type) .accordion-header").attr("aria-expanded", "false");
+  $(".accordion .accordion-item:not(:first-of-type) .accordion-content").hide();
+
   $(".accordion-header").click(function () {
+    let targetContent = $(this).attr("aria-controls");
+    let classes = $(".bkg-climb").attr("class").split(" ");
+    $(".bkg-climb").removeClass(classes[1]);
+    $(".bkg-climb").addClass(targetContent);
+    $(".accordion-content").not($(this).next(".accordion-content")).slideUp();
+    $(".accordion-header").not($(this)).attr("aria-expanded", "false");
     $(this).next(".accordion-content").slideToggle();
     $(this).attr("aria-expanded", function (_, attr) {
       return attr == "true" ? "false" : "true";
